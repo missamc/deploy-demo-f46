176253602d382c65f591cfbe8699010a8fc66104
arianna <missamc36@yahoo.com>
2024-03-28 14:00:17 -0700

    deploy-demo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.listen(4000, () => console.log('Running on port 4000'))
+app.listen(4500, () => console.log('Running on port 4500'))
 
 
 
